8dfb709f0811de91c2d3e4326593c7608306c634
Zhelyazko Georgiev <jeko@codific.eu>
2020-02-21 14:06:32 +0200

    Date picker week order changed to start from Monday.

--- src/Codific.Mvc567.UI/Scripts/js/shared/formpickers.js
+++ src/Codific.Mvc567.UI/Scripts/js/shared/formpickers.js
@@ -11,28 +11,34 @@
   if ($(".datepicker-popup").length) {
     $('.datepicker-popup').datepicker({
         enableOnReadonly: true,
-        todayHighlight: true
+        todayHighlight: true,
+        weekStart: 1
     });
   }
   if ($("#inline-datepicker").length) {
     $('#inline-datepicker').datepicker({
       enableOnReadonly: true,
       todayHighlight: true,
+      weekStart: 1
       // language: 'en' For English Language
     });
   }
   if ($(".datepicker-autoclose").length) {
     $('.datepicker-autoclose').datepicker({
+      weekStart: 1,
       autoclose: true
     });
   }
   if ($('input[name="date-range"]').length) {
-    $('input[name="date-range"]').daterangepicker();
+    $('input[name="date-range"]').daterangepicker({
+      weekStart: 1
+    });
   }
   if ($('input[name="date-time-range"]').length) {
     $('input[name="date-time-range"]').daterangepicker({
       timePicker: true,
       timePickerIncrement: 30,
+      weekStart: 1,
       locale: {
         format: 'MM/DD/YYYY h:mm A'
       }
